c41d0455c1c77befba7b8f185978443787216163
Tu Huy Nguyen <dragon10x0408@gmail.com>
2022-09-30 13:12:30 +0700

    gio thoi vi vu
    Co-authored-by: huytunguyenls <tnguyen@lsscontract.com>

--- src/reducers.js
+++ src/reducers.js
@@ -2,9 +2,7 @@ import history from 'utilities/history';
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
-/**
- * Merges the main reducer with the router state and dynamically injected reducers
- */
+// Merges the main reducer with the router state and dynamically injected reducers
 export default function createReducer(injectedReducers = {}) {
   return combineReducers({
     router: connectRouter(history),
